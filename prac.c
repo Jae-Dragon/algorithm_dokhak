4efa25525c516171ea844b000e0bf223ee17b830
MiniPenguin <wodyd818@naver.com>
2023-05-19 15:39:18 +0900

    test_commit

---
+++ prac.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+   while(a!=b)
+   {
+        if(a > b)
+        {
+            a -= b;
+        }
+        else
+        {
+            b -= a;
+        }
+
+   }
+   printf("%d", a);
+
+    return 0;
+}
