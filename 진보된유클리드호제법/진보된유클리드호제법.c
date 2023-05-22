408ce6a29eed0a2f618ed2db5f7a11b64c9e9f25
MiniPenguin <wodyd818@naver.com>
2023-05-22 12:19:06 +0900

    second

--- 진보된유클리드호제법/진보된유클리드호제법.c
+++ 진보된유클리드호제법/진보된유클리드호제법.c
@@ -8,19 +8,23 @@ int main()
     int a, b;
     scanf("%d %d", &a, &b);
 
-   while(a!=b)
+   while(a%b != 0 && b%a != 0)
    {
         if(a > b)
         {
-            a -= b;
+            a %= b;
         }
         else
         {
-            b -= a;
+            b %= a;
         }
 
    }
-   printf("%d", a);
+   int t = 0;
+   if(a > b) t = b;
+   else t = a;
+
+   printf("%d", t);
 
     return 0;
 }
